aa4452e87de551cd607e8ffb8d7b7b8838d12767
soenkeba <sbartinfo@gmx.de>
2021-10-15 07:38:20 +0200

    testmetadaten

--- src/database.js
+++ src/database.js
@@ -1,26 +1,69 @@
 module.exports = {
-  "1": {
-    "description": "Friendly OpenSea Creature that enjoys long swims in the ocean.", 
-    "external_url": "https://openseacreatures.io/3", 
-    "image": "https://storage.googleapis.com/opensea-prod.appspot.com/puffs/3.png", 
-    "name": "Dave Starbelly",
-    "attributes": [ ], 
-  }
-  ,
-  "2": {
-    "name": "Flerp Derp",
-    "birthday": "01-08"
-  },
-  "3": {
-    "name": "Buttle McFuddle",
-    "birthday": "01-22"
-  },
-  "4": {
-    "name": "Quiero Pancakes",
-    "birthday": "03-04"
-  },
-  "5": {
-    "name": "Guino the Penguin",
-    "birthday": "03-09"
-  }
+	"1": {
+		"description": "TEST ZENTRAL Friendly OpenSea Creature that enjoys long swims in the ocean.",
+		"external_url": "https://openseacreatures.io/3",
+		"image": "https://storage.googleapis.com/opensea-prod.appspot.com/puffs/3.png",
+		"name": "Dave Starbelly",
+		"attributes": []
+	},
+	"2": {
+		"name": "TEST ZENTRAL Molecular Configuration in Sodium Thymonucleate",
+		"description": "Today, Rosalind Franklin is mainly known as the woman behind the discovery of the structure of DNA. She was not able to be considered for the Nobel prize due to her early death at the age of 37 years. ",
+		"external_link": "https://FALSCHERLINKwww.nature.com/articles/171740a0",
+		"image": "https://lh3.googleusercontent.com/9_PipZDTa8tmwdRiUlFfMnwO6oLkloVyPUKtvGtzlKCPAP9hFt8wTOAnXACL2UWHy6It0xzbz0bcOmORAmdflX_MokHu5htUe0CE",
+		"animation_url": null
+	},
+	"3": {
+		"name" : "TEST ZENTRAL ALTE ZENTRAL METADATEN Long Is the Coast of Britain? Statistical Self-Similarity and Fractional Dimension",
+		"description": "TestIn this paper, Mandelbrot discusses self-similar curves. These curves are examples of fractals, although Mandelbrot does not use this term in the paper, as he did not coin it until 1975. The paper is one of Mandelbrot's first publications on the topic of fractals.",
+		"external_link": "http://li.mit.edu/Stuff/CNSE/Paper/Mandelbrot67Science.pdf",
+		"image": "https://lh3.googleusercontent.com/m2xQYd19pql9HHGz6G_I1KExsh3KP_va-z-Qkr1ufOE5UurVipiXHtKEt5Gel-DZ-7OPIuIgmyd8npzBTtMXd-OE2FuZ0OWtGhsM",
+		"animation_url": null
+	},
+	"4": {
+		image_url: "ipfs://bafybeifl6plqbuk5rflpeedeznlxs2gdj2j6d6xf52a2jnk4v6l4bpypwe/image",
+		name: "TEST ZENTRAL NEUE DEZENTRALE METADATEN How Long Is the Coast of Britain? Statistical Self-Similarity and Fractional Dimension",
+		description: "In this paper, Mandelbrot discusses self-similar curves. These curves are examples of fractals, although Mandelbrot does not use this term in the paper, as he did not coin it until 1975. The paper is one of Mandelbrot's first publications on the topic of fractals.",
+		external_url: "http://li.mit.edu/Stuff/CNSE/Paper/Mandelbrot67Science.pdf",
+		properties: [{
+				trait_type: "IPFS",
+				value: "QmYPdo1xF7Vsj6uH1KdAUHJjWaF1zYKaARG4tJBnSHxpZ9"
+			},
+			{
+				trait_type: "ISCC",
+				value: "KAD47FAO76UMQW7NZJUS45GDY2D7Y5ENIWTBABDTT4I7QLDEEWZP6VY"
+			},
+			{
+				trait_type: "REFISO",
+				value: "Mandelbrot, B. How Long Is the Coast of Britain? Statistical Self-Similarity and Fractional Dimension. Science, 1967, vol. 156, no. 3775., pp. 636-638"
+			},
+			{
+				trait_type: "TITLE",
+				value: "How Long Is the Coast of Britain? Statistical Self-Similarity and Fractional Dimension"
+			}
+		]
+	},
+	"5": {
+		"description": "Komplett selbst gemachte Metadaten",
+		"external_url": "akasha.world",
+		"image": "ipfs://Qmc5DMotaCgtZad3v956PoKiNEsHptoUova5MqexZqS9dA",
+		"name": "Komplett selbstgemachte Metadaten nach Open Sea standard",
+		"attributes": {
+			"waschmittel": "nein",
+			"raccoon": "ja",
+			"zecke": "ausm wald 3373"
+		},
+	},
+
+	"01559ae4021a9c84fa506cb76691054c2b92f655222cde2437d77ed21c083c4d": {
+		"description": "Selbstgemachte Metadaten ganz lange ID, wie ipfs",
+		"external_url": "akasha.world",
+		"image": "ipfs://Qmc5DMotaCgtZad3v956PoKiNEsHptoUova5MqexZqS9dA",
+		"name": "Komplett selbstgemachte Metadaten nach Open Sea standard",
+		"attributes": {
+			"waschmittel": "nein",
+			"raccoon": "ja",
+			"zecke": "ausm wald 3373"
+		}
+	}
 }
